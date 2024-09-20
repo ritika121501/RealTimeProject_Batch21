37b30963d8960075af22e8f1dddf4f798d79da17
Prashant Jha <jha_prashant@outlook.com>
2024-09-20 05:53:07 +0530

    Order tables completed- Migration pending

---
+++ RealTimeProject_Batch21/Models/OrderDetail.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace RealTimeProject_Batch21.Models
+{
+    public class OrderDetail
+    {
+        public int OrderDetailId { get; set; }
+        public string OrderHeaderId {  get; set; }
+        [ForeignKey("OrderHeaderId")]
+        public OrderHeader OrderHeader { get; set; }
+
+        [Required]
+        public int ProductId { get; set; }
+        [ForeignKey("ProductId")]
+        public Product Product { get; set; }
+        public int Count { get; set; }
+        public double Price { get; set; }
+    }
+}
---
+++ RealTimeProject_Batch21/Models/OrderHeader.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace RealTimeProject_Batch21.Models
+{
+    public class OrderHeader
+    {
+        public int OrderHeaderId { get; set; }
+
+        public string ApplicationUserId {  get; set; }
+        [ForeignKey("ApplicationUserId")]
+        public ApplicationUser ApplicationUser { get; set; }
+
+        public DateTime OrderDate { get; set; } 
+
+        public DateTime ShippingDate { get; set; }  
+        public double OrderTotal { get; set; }
+        public string? OrderStatus { get; set; }
+        public string? PaymentStatus { get; set; }
+    }
+}
